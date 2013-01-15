34aac04277ec53855d1efcc36baad5edfef4a5eb
Tyler Savery <tyler@Tylers-MacBook.local>
2013-01-15 13:25:06 -0500

    js cleanup

--- js/kropr.js
+++ js/kropr.js
@@ -18,36 +18,36 @@ Kropr.settings.debug = true;
 
 
 //init the app
-Kropr.init = function() {
+Kropr.init = function () {
 
-	Kropr.resetProperties();
-	//add event listeners
-	Kropr.bindListeners();
+    Kropr.resetProperties();
+    //add event listeners
+    Kropr.bindListeners();
 
 }
 
 //reset everything
 Kropr.resetProperties = function () {
 
-	//instantiate properties
-	Kropr.canvas = null;
-	Kropr.ctx = null;
-	Kropr.image = null;
-	Kropr.iMouseX = 1;
-	Kropr.iMouseY = 1;
-	Kropr.theSelection = null;
+    //instantiate properties
+    Kropr.canvas = null;
+    Kropr.ctx = null;
+    Kropr.image = null;
+    Kropr.iMouseX = 1;
+    Kropr.iMouseY = 1;
+    Kropr.theSelection = null;
 
-	$("#" + Kropr.settings.results_id).fadeOut(300);
+    $("#" + Kropr.settings.results_id).fadeOut(300);
 
 }
 
 //when a file is uploaded
-Kropr.fileUploaded = function(id) {
+Kropr.fileUploaded = function (id) {
 
-	//give it a clean slate
-	Kropr.resetProperties();
+    //give it a clean slate
+    Kropr.resetProperties();
 
-	//fade in preview container
+    //fade in preview container
     $("#" + Kropr.settings.container_id).fadeIn(300);
 
     //get user selected file
@@ -62,28 +62,28 @@ Kropr.fileUploaded = function(id) {
 
     //init file reader
     var reader = new FileReader();
-    
-    //when the file reader is ready
-    reader.onload = function(e){
 
-    	//init image
+    //when the file reader is ready
+    reader.onload = function (e) {
+
+        //init image
         Kropr.image = new Image();
         Kropr.image.src = e.target.result;
 
         //when the image is loaded
-        Kropr.image.onload = function(){
+        Kropr.image.onload = function () {
 
-        	//console log information about the image if debug is set to true
-        	if(Kropr.settings.debug) {
-	            console.log('Name: ' + file.name);
-	            console.log('Type: ' + file.type);
-	            console.log('Width: ' + Kropr.image.naturalWidth);
-	            console.log('Height: ' + Kropr.image.naturalHeight);
-	            console.log('Filesize: ' + Kropr.bytesToSize(file.size));
-        	}
+            //console log information about the image if debug is set to true
+            if (Kropr.settings.debug) {
+                console.log('Name: ' + file.name);
+                console.log('Type: ' + file.type);
+                console.log('Width: ' + Kropr.image.naturalWidth);
+                console.log('Height: ' + Kropr.image.naturalHeight);
+                console.log('Filesize: ' + Kropr.bytesToSize(file.size));
+            }
 
-        	//create the canvas
-            $("#" + Kropr.settings.container_id).html('<canvas width="'+ Kropr.image.naturalWidth +'" height="'+ Kropr.image.naturalHeight +'" id="kropr_preview" /> ')
+            //create the canvas
+            $("#" + Kropr.settings.container_id).html('<canvas width="' + Kropr.image.naturalWidth + '" height="' + Kropr.image.naturalHeight + '" id="kropr_preview" /> ')
             //create the crop button
             $("#" + Kropr.settings.container_id).append('<a href="#" id="kropr_complete" >Crop</a>');
 
@@ -94,15 +94,15 @@ Kropr.fileUploaded = function(id) {
             Kropr.bindLateListeners();
 
             //clear the canvas
-            Kropr.ctx.clearRect(0, 0, Kropr.ctx.canvas.width, Kropr.ctx.canvas.height);            
-            
+            Kropr.ctx.clearRect(0, 0, Kropr.ctx.canvas.width, Kropr.ctx.canvas.height);
+
             //setup the image on the canvas
             Kropr.ctx.drawImage(Kropr.image, 0, 0, Kropr.ctx.canvas.width, Kropr.ctx.canvas.height);
 
             //setup the overlay
             Kropr.ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
             Kropr.ctx.fillRect(0, 0, Kropr.ctx.canvas.width, Kropr.ctx.canvas.height);
-            
+
             //setup the initial selection
             Kropr.theSelection = new Kropr.Selection(15, 15, Kropr.ctx.canvas.width - 30, Kropr.ctx.canvas.height - 30);
 
@@ -118,7 +118,7 @@ Kropr.fileUploaded = function(id) {
 }
 
 //draw the canvas
-Kropr.drawScene = function() { 
+Kropr.drawScene = function () {
 
     Kropr.ctx.clearRect(0, 0, Kropr.ctx.canvas.width, Kropr.ctx.canvas.height); // clear canvas
 
@@ -134,184 +134,179 @@ Kropr.drawScene = function() {
 }
 
 //crop the image / show the results
-Kropr.getResults = function() {
+Kropr.getResults = function () {
 
-	//create temp variables
+    //create temp variables
     var temp_ctx, temp_canvas;
-    
+
     //setup temp canvas 
     temp_canvas = document.createElement('canvas');
     temp_ctx = temp_canvas.getContext('2d');
     temp_canvas.width = Kropr.theSelection.w;
     temp_canvas.height = Kropr.theSelection.h;
     temp_ctx.drawImage(Kropr.image, Kropr.theSelection.x, Kropr.theSelection.y, Kropr.theSelection.w, Kropr.theSelection.h, 0, 0, Kropr.theSelection.w, Kropr.theSelection.h);
-    
+
     //get image data
     var vData = temp_canvas.toDataURL();
 
     //set results image with date
     $("#" + Kropr.settings.results_id).attr('src', vData);
-	$("#" + Kropr.settings.results_id).show(300);
+    $("#" + Kropr.settings.results_id).show(300);
     $("#" + Kropr.settings.container_id).hide(300);
 
 }
 
 //bind listeners
-Kropr.bindListeners = function() {
+Kropr.bindListeners = function () {
 
-	//When a file is selected
-	$("#" + Kropr.settings.upload_id).change(function() {
+    //When a file is selected
+    $("#" + Kropr.settings.upload_id).change(function () {
 
-		Kropr.fileUploaded(Kropr.settings.upload_id);
+        Kropr.fileUploaded(Kropr.settings.upload_id);
 
-	});
+    });
 
 }
 
 //bind listeners to objects added later
-Kropr.bindLateListeners = function() {	
+Kropr.bindLateListeners = function () {
 
-	//when mouse moves
-	$('#kropr_preview').mousemove(function(e) { // binding mouse move event
-	    
-	    var canvasOffset = $("#kropr_preview").offset();
+    //when mouse moves
+    $('#kropr_preview').mousemove(function (e) { // binding mouse move event
 
-	    Kropr.iMouseX = Math.floor(e.pageX - canvasOffset.left);
-	    Kropr.iMouseY = Math.floor(e.pageY - canvasOffset.top);
+        var canvasOffset = $("#kropr_preview").offset();
 
-	    // in case of drag of whole selector
-	    if (Kropr.theSelection.bDragAll) {
-	        Kropr.theSelection.x = Kropr.iMouseX - Kropr.theSelection.px;
-	        Kropr.theSelection.y = Kropr.iMouseY - Kropr.theSelection.py;
-	    }
+        Kropr.iMouseX = Math.floor(e.pageX - canvasOffset.left);
+        Kropr.iMouseY = Math.floor(e.pageY - canvasOffset.top);
 
-	    for (i = 0; i < 4; i++) {
-	        Kropr.theSelection.bHow[i] = false;
-	        Kropr.theSelection.iCSize[i] = Kropr.theSelection.csize;
-	    }
+        // in case of drag of whole selector
+        if (Kropr.theSelection.bDragAll) {
+            Kropr.theSelection.x = Kropr.iMouseX - Kropr.theSelection.px;
+            Kropr.theSelection.y = Kropr.iMouseY - Kropr.theSelection.py;
+        }
 
-	    // hovering over resize cubes
-	    if (Kropr.iMouseX > Kropr.theSelection.x - Kropr.theSelection.csizeh && Kropr.iMouseX < Kropr.theSelection.x + Kropr.theSelection.csizeh &&
-	        Kropr.iMouseY > Kropr.theSelection.y - Kropr.theSelection.csizeh && Kropr.iMouseY < Kropr.theSelection.y + Kropr.theSelection.csizeh) {
+        for (i = 0; i < 4; i++) {
+            Kropr.theSelection.bHow[i] = false;
+            Kropr.theSelection.iCSize[i] = Kropr.theSelection.csize;
+        }
 
-	        Kropr.theSelection.bHow[0] = true;
-	        Kropr.theSelection.iCSize[0] = Kropr.theSelection.csizeh;
-	    }
-	    if (Kropr.iMouseX > Kropr.theSelection.x + Kropr.theSelection.w-Kropr.theSelection.csizeh && Kropr.iMouseX < Kropr.theSelection.x + Kropr.theSelection.w + Kropr.theSelection.csizeh &&
-	        Kropr.iMouseY > Kropr.theSelection.y - Kropr.theSelection.csizeh && Kropr.iMouseY < Kropr.theSelection.y + Kropr.theSelection.csizeh) {
+        // hovering over resize cubes
+        if (Kropr.iMouseX > Kropr.theSelection.x - Kropr.theSelection.csizeh && Kropr.iMouseX < Kropr.theSelection.x + Kropr.theSelection.csizeh && Kropr.iMouseY > Kropr.theSelection.y - Kropr.theSelection.csizeh && Kropr.iMouseY < Kropr.theSelection.y + Kropr.theSelection.csizeh) {
 
-	        Kropr.theSelection.bHow[1] = true;
-	        Kropr.theSelection.iCSize[1] = Kropr.theSelection.csizeh;
-	    }
-	    if (Kropr.iMouseX > Kropr.theSelection.x + Kropr.theSelection.w-Kropr.theSelection.csizeh && Kropr.iMouseX < Kropr.theSelection.x + Kropr.theSelection.w + Kropr.theSelection.csizeh &&
-	        Kropr.iMouseY > Kropr.theSelection.y + Kropr.theSelection.h-Kropr.theSelection.csizeh && Kropr.iMouseY < Kropr.theSelection.y + Kropr.theSelection.h + Kropr.theSelection.csizeh) {
+            Kropr.theSelection.bHow[0] = true;
+            Kropr.theSelection.iCSize[0] = Kropr.theSelection.csizeh;
+        }
+        if (Kropr.iMouseX > Kropr.theSelection.x + Kropr.theSelection.w - Kropr.theSelection.csizeh && Kropr.iMouseX < Kropr.theSelection.x + Kropr.theSelection.w + Kropr.theSelection.csizeh && Kropr.iMouseY > Kropr.theSelection.y - Kropr.theSelection.csizeh && Kropr.iMouseY < Kropr.theSelection.y + Kropr.theSelection.csizeh) {
 
-	        Kropr.theSelection.bHow[2] = true;
-	        Kropr.theSelection.iCSize[2] = Kropr.theSelection.csizeh;
-	    }
-	    if (Kropr.iMouseX > Kropr.theSelection.x - Kropr.theSelection.csizeh && Kropr.iMouseX < Kropr.theSelection.x + Kropr.theSelection.csizeh &&
-	        Kropr.iMouseY > Kropr.theSelection.y + Kropr.theSelection.h-Kropr.theSelection.csizeh && Kropr.iMouseY < Kropr.theSelection.y + Kropr.theSelection.h + Kropr.theSelection.csizeh) {
+            Kropr.theSelection.bHow[1] = true;
+            Kropr.theSelection.iCSize[1] = Kropr.theSelection.csizeh;
+        }
+        if (Kropr.iMouseX > Kropr.theSelection.x + Kropr.theSelection.w - Kropr.theSelection.csizeh && Kropr.iMouseX < Kropr.theSelection.x + Kropr.theSelection.w + Kropr.theSelection.csizeh && Kropr.iMouseY > Kropr.theSelection.y + Kropr.theSelection.h - Kropr.theSelection.csizeh && Kropr.iMouseY < Kropr.theSelection.y + Kropr.theSelection.h + Kropr.theSelection.csizeh) {
 
-	        Kropr.theSelection.bHow[3] = true;
-	        Kropr.theSelection.iCSize[3] = Kropr.theSelection.csizeh;
-	    }
+            Kropr.theSelection.bHow[2] = true;
+            Kropr.theSelection.iCSize[2] = Kropr.theSelection.csizeh;
+        }
+        if (Kropr.iMouseX > Kropr.theSelection.x - Kropr.theSelection.csizeh && Kropr.iMouseX < Kropr.theSelection.x + Kropr.theSelection.csizeh && Kropr.iMouseY > Kropr.theSelection.y + Kropr.theSelection.h - Kropr.theSelection.csizeh && Kropr.iMouseY < Kropr.theSelection.y + Kropr.theSelection.h + Kropr.theSelection.csizeh) {
 
-	    // in case of dragging of resize cubes
-	    var iFW, iFH;
-	    if (Kropr.theSelection.bDrag[0]) {
-	        var iFX = Kropr.iMouseX - Kropr.theSelection.px;
-	        var iFY = Kropr.iMouseY - Kropr.theSelection.py;
-	        iFW = Kropr.theSelection.w + Kropr.theSelection.x - iFX;
-	        iFH = Kropr.theSelection.h + Kropr.theSelection.y - iFY;
-	    }
-	    if (Kropr.theSelection.bDrag[1]) {
-	        var iFX = Kropr.theSelection.x;
-	        var iFY = Kropr.iMouseY - Kropr.theSelection.py;
-	        iFW = Kropr.iMouseX - Kropr.theSelection.px - iFX;
-	        iFH = Kropr.theSelection.h + Kropr.theSelection.y - iFY;
-	    }
-	    if (Kropr.theSelection.bDrag[2]) {
-	        var iFX = Kropr.theSelection.x;
-	        var iFY = Kropr.theSelection.y;
-	        iFW = Kropr.iMouseX - Kropr.theSelection.px - iFX;
-	        iFH = Kropr.iMouseY - Kropr.theSelection.py - iFY;
-	    }
-	    if (Kropr.theSelection.bDrag[3]) {
-	        var iFX = Kropr.iMouseX - Kropr.theSelection.px;
-	        var iFY = Kropr.theSelection.y;
-	        iFW = Kropr.theSelection.w + Kropr.theSelection.x - iFX;
-	        iFH = Kropr.iMouseY - Kropr.theSelection.py - iFY;
-	    }
+            Kropr.theSelection.bHow[3] = true;
+            Kropr.theSelection.iCSize[3] = Kropr.theSelection.csizeh;
+        }
 
-	    if (iFW > Kropr.theSelection.csizeh * 2 && iFH > Kropr.theSelection.csizeh * 2) {
-	        Kropr.theSelection.w = iFW;
-	        Kropr.theSelection.h = iFH;
+        // in case of dragging of resize cubes
+        var iFW, iFH;
+        if (Kropr.theSelection.bDrag[0]) {
+            var iFX = Kropr.iMouseX - Kropr.theSelection.px;
+            var iFY = Kropr.iMouseY - Kropr.theSelection.py;
+            iFW = Kropr.theSelection.w + Kropr.theSelection.x - iFX;
+            iFH = Kropr.theSelection.h + Kropr.theSelection.y - iFY;
+        }
+        if (Kropr.theSelection.bDrag[1]) {
+            var iFX = Kropr.theSelection.x;
+            var iFY = Kropr.iMouseY - Kropr.theSelection.py;
+            iFW = Kropr.iMouseX - Kropr.theSelection.px - iFX;
+            iFH = Kropr.theSelection.h + Kropr.theSelection.y - iFY;
+        }
+        if (Kropr.theSelection.bDrag[2]) {
+            var iFX = Kropr.theSelection.x;
+            var iFY = Kropr.theSelection.y;
+            iFW = Kropr.iMouseX - Kropr.theSelection.px - iFX;
+            iFH = Kropr.iMouseY - Kropr.theSelection.py - iFY;
+        }
+        if (Kropr.theSelection.bDrag[3]) {
+            var iFX = Kropr.iMouseX - Kropr.theSelection.px;
+            var iFY = Kropr.theSelection.y;
+            iFW = Kropr.theSelection.w + Kropr.theSelection.x - iFX;
+            iFH = Kropr.iMouseY - Kropr.theSelection.py - iFY;
+        }
 
-	        Kropr.theSelection.x = iFX;
-	        Kropr.theSelection.y = iFY;
-	    }
+        if (iFW > Kropr.theSelection.csizeh * 2 && iFH > Kropr.theSelection.csizeh * 2) {
+            Kropr.theSelection.w = iFW;
+            Kropr.theSelection.h = iFH;
 
-	    Kropr.drawScene();
-	});
+            Kropr.theSelection.x = iFX;
+            Kropr.theSelection.y = iFY;
+        }
+
+        Kropr.drawScene();
+    });
 
 
-	//when mouse goes down
-	$('#kropr_preview').mousedown(function(e) {
+    //when mouse goes down
+    $('#kropr_preview').mousedown(function (e) {
 
-	    var canvasOffset = $("#kropr_preview").offset();
-	    Kropr.iMouseX = Math.floor(e.pageX - canvasOffset.left);
-	    Kropr.iMouseY = Math.floor(e.pageY - canvasOffset.top);
+        var canvasOffset = $("#kropr_preview").offset();
+        Kropr.iMouseX = Math.floor(e.pageX - canvasOffset.left);
+        Kropr.iMouseY = Math.floor(e.pageY - canvasOffset.top);
 
-	    Kropr.theSelection.px = Kropr.iMouseX - Kropr.theSelection.x;
-	    Kropr.theSelection.py = Kropr.iMouseY - Kropr.theSelection.y;
+        Kropr.theSelection.px = Kropr.iMouseX - Kropr.theSelection.x;
+        Kropr.theSelection.py = Kropr.iMouseY - Kropr.theSelection.y;
 
-	    if (Kropr.theSelection.bHow[0]) {
-	        Kropr.theSelection.px = Kropr.iMouseX - Kropr.theSelection.x;
-	        Kropr.theSelection.py = Kropr.iMouseY - Kropr.theSelection.y;
-	    }
-	    if (Kropr.theSelection.bHow[1]) {
-	        Kropr.theSelection.px = Kropr.iMouseX - Kropr.theSelection.x - Kropr.theSelection.w;
-	        Kropr.theSelection.py = Kropr.iMouseY - Kropr.theSelection.y;
-	    }
-	    if (Kropr.theSelection.bHow[2]) {
-	        Kropr.theSelection.px = Kropr.iMouseX - Kropr.theSelection.x - Kropr.theSelection.w;
-	        Kropr.theSelection.py = Kropr.iMouseY - Kropr.theSelection.y - Kropr.theSelection.h;
-	    }
-	    if (Kropr.theSelection.bHow[3]) {
-	        Kropr.theSelection.px = Kropr.iMouseX - Kropr.theSelection.x;
-	        Kropr.theSelection.py = Kropr.iMouseY - Kropr.theSelection.y - Kropr.theSelection.h;
-	    }
+        if (Kropr.theSelection.bHow[0]) {
+            Kropr.theSelection.px = Kropr.iMouseX - Kropr.theSelection.x;
+            Kropr.theSelection.py = Kropr.iMouseY - Kropr.theSelection.y;
+        }
+        if (Kropr.theSelection.bHow[1]) {
+            Kropr.theSelection.px = Kropr.iMouseX - Kropr.theSelection.x - Kropr.theSelection.w;
+            Kropr.theSelection.py = Kropr.iMouseY - Kropr.theSelection.y;
+        }
+        if (Kropr.theSelection.bHow[2]) {
+            Kropr.theSelection.px = Kropr.iMouseX - Kropr.theSelection.x - Kropr.theSelection.w;
+            Kropr.theSelection.py = Kropr.iMouseY - Kropr.theSelection.y - Kropr.theSelection.h;
+        }
+        if (Kropr.theSelection.bHow[3]) {
+            Kropr.theSelection.px = Kropr.iMouseX - Kropr.theSelection.x;
+            Kropr.theSelection.py = Kropr.iMouseY - Kropr.theSelection.y - Kropr.theSelection.h;
+        }
 
-	    if (Kropr.iMouseX > Kropr.theSelection.x + Kropr.theSelection.csizeh && Kropr.iMouseX < Kropr.theSelection.x+Kropr.theSelection.w - Kropr.theSelection.csizeh &&
-	        Kropr.iMouseY > Kropr.theSelection.y + Kropr.theSelection.csizeh && Kropr.iMouseY < Kropr.theSelection.y+Kropr.theSelection.h - Kropr.theSelection.csizeh) {
+        if (Kropr.iMouseX > Kropr.theSelection.x + Kropr.theSelection.csizeh && Kropr.iMouseX < Kropr.theSelection.x + Kropr.theSelection.w - Kropr.theSelection.csizeh && Kropr.iMouseY > Kropr.theSelection.y + Kropr.theSelection.csizeh && Kropr.iMouseY < Kropr.theSelection.y + Kropr.theSelection.h - Kropr.theSelection.csizeh) {
 
-	        Kropr.theSelection.bDragAll = true;
-	    }
+            Kropr.theSelection.bDragAll = true;
+        }
 
-	    for (i = 0; i < 4; i++) {
-	        if (Kropr.theSelection.bHow[i]) {
-	            Kropr.theSelection.bDrag[i] = true;
-	        }
-	    }
-	});
+        for (i = 0; i < 4; i++) {
+            if (Kropr.theSelection.bHow[i]) {
+                Kropr.theSelection.bDrag[i] = true;
+            }
+        }
+    });
 
-	//when mouse is released
-	$('#kropr_preview').mouseup(function(e) { // binding mouseup event
+    //when mouse is released
+    $('#kropr_preview').mouseup(function (e) { // binding mouseup event
 
-	    Kropr.theSelection.bDragAll = false;
+        Kropr.theSelection.bDragAll = false;
 
-	    for (i = 0; i < 4; i++) {
-	        Kropr.theSelection.bDrag[i] = false;
-	    }
-	    Kropr.theSelection.px = 0;
-	    Kropr.theSelection.py = 0;
-	});
+        for (i = 0; i < 4; i++) {
+            Kropr.theSelection.bDrag[i] = false;
+        }
+        Kropr.theSelection.px = 0;
+        Kropr.theSelection.py = 0;
+    });
 
 
-	//when crop button is clicked
-	$("#kropr_complete").click(function(){
-	    Kropr.getResults();
-	});
+    //when crop button is clicked
+    $("#kropr_complete").click(function () {
+        Kropr.getResults();
+    });
 
 
 }
@@ -323,7 +318,7 @@ Kropr.bindLateListeners = function() {
 
 /*  Selection Class */
 
-Kropr.Selection = function (x, y, w, h){
+Kropr.Selection = function (x, y, w, h) {
     this.x = x; // initial positions
     this.y = y;
     this.w = w; // and size
@@ -341,7 +336,7 @@ Kropr.Selection = function (x, y, w, h){
     this.bDragAll = false; // drag whole selection
 }
 
-Kropr.Selection.prototype.draw = function(){
+Kropr.Selection.prototype.draw = function () {
 
     Kropr.ctx.strokeStyle = '#000';
     Kropr.ctx.lineWidth = 2;
@@ -366,7 +361,7 @@ Kropr.Selection.prototype.draw = function(){
 /* Utility Function */
 /* ---------------- */
 
-Kropr.bytesToSize = function(bytes) {
+Kropr.bytesToSize = function (bytes) {
     var sizes = ['Bytes', 'KB', 'MB'];
     if (bytes == 0) return 'n/a';
 
@@ -380,8 +375,8 @@ Kropr.bytesToSize = function(bytes) {
 /* Dom Loaded */
 /* ---------- */
 
-$(document).ready(function() {
+$(document).ready(function () {
 
-	Kropr.init();
+    Kropr.init();
 
 });
